e67f54eb472b8970738327b3e2ae2e530b673ed0
David Hill <dhill1678@yahoo.com>
2015-01-18 20:17:08 -0500

    login added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :recipes
 
   get 'recipes/' =>'recipes#index'
 
-  root to: 'welcome#index'
+  resources :dashboard
+  root to: "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
