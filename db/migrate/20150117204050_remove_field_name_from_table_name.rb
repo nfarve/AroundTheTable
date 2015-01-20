a76caa872692c814b3dab688a1ee367111bf3239
David Hill <dhill1678@yahoo.com>
2015-01-20 15:46:31 -0500

    attempted precompile error fix 9

--- db/migrate/20150117204050_remove_field_name_from_table_name.rb
+++
@@ -1,7 +0,0 @@
-class RemoveFieldNameFromTableName < ActiveRecord::Migration
-  def change
-  	change_table :recipes do |t|
-    	t.remove :ingredients_main, :ingredients_veg, :ingredients_spice, :ingredients_misc
-    end
-  end
-end
